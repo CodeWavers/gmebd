a0573e9067d46fec1ca67db68e929e60b989dce8
CodeWavers <76722789+CodeWavers@users.noreply.github.com>
2021-07-12 15:27:58 +0600

    Revert "Fariya"

--- application/controllers/Admin_dashboard.php
+++ application/controllers/Admin_dashboard.php
@@ -645,47 +645,6 @@ class Admin_dashboard extends CI_Controller {
         $content = $CI->lreport->purchase_warrenty_report_category_wise($links, $config["per_page"], $page);
         $this->template->full_admin_html_view($content);
     }
-
-     public function purchase_expired_report_category_wise() {
-        $CI = & get_instance();
-        $CI->load->library('lreport');
-        $this->auth->check_admin_auth();
-
-        #
-        #pagination starts
-        #
-        $config["base_url"] = base_url('Admin_dashboard/purchase_expired_report_category_wise/');
-        $config["total_rows"] = $this->Reports->purchase_report_category_wise_count();
-        $config["per_page"] = 50;
-        $config["uri_segment"] = 3;
-        $config["num_links"] = 5;
-        /* This Application Must Be Used With BootStrap 3 * */
-        $config['full_tag_open'] = "<ul class='pagination'>";
-        $config['full_tag_close'] = "</ul>";
-        $config['num_tag_open'] = '<li>';
-        $config['num_tag_close'] = '</li>';
-        $config['cur_tag_open'] = "<li class='disabled'><li class='active'><a href='#'>";
-        $config['cur_tag_close'] = "<span class='sr-only'></span></a></li>";
-        $config['next_tag_open'] = "<li>";
-        $config['next_tag_close'] = "</li>";
-        $config['prev_tag_open'] = "<li>";
-        $config['prev_tagl_close'] = "</li>";
-        $config['first_tag_open'] = "<li>";
-        $config['first_tagl_close'] = "</li>";
-        $config['last_tag_open'] = "<li>";
-        $config['last_tagl_close'] = "</li>";
-        /* ends of bootstrap */
-        $this->pagination->initialize($config);
-        $page = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
-        $links = $this->pagination->create_links();
-
-        #
-        #pagination ends
-        #
-
-        $content = $CI->lreport->purchase_expired_report_category_wise($links, $config["per_page"], $page);
-        $this->template->full_admin_html_view($content);
-    }
     public function purchase_report_shelf_wise() {
         $CI = & get_instance();
         $CI->load->library('lreport');
@@ -825,15 +784,6 @@ class Admin_dashboard extends CI_Controller {
         $from_date = $this->input->post('from_date',TRUE);
         $to_date   = $this->input->post('to_date',TRUE);
         $content   = $this->lreport->filter_purchase_warrenty_report_category_wise($category, $from_date, $to_date);
-        $this->template->full_admin_html_view($content);
-    }
-    public function filter_purchase_expired_report_category_wise() {
-        $CI = & get_instance();
-        $CI->load->library('lreport');
-        $category  = $this->input->post('category',TRUE);
-        $from_date = $this->input->post('from_date',TRUE);
-        $to_date   = $this->input->post('to_date',TRUE);
-        $content   = $this->lreport->filter_purchase_expired_report_category_wise($category, $from_date, $to_date);
         $this->template->full_admin_html_view($content);
     }
     public function filter_purchase_report_shelf_wise() {
